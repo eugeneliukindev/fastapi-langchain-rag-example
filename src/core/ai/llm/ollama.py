7dc32221a017f52c0cc0659165d93debc0f4bdf7
eugene <eugeneliukin.dev@gmail.com>
2026-05-10 22:08:54 +0300

    fix config attributes

--- src/core/ai/llm/ollama.py
+++ src/core/ai/llm/ollama.py
@@ -3,6 +3,6 @@ from langchain_ollama import ChatOllama
 from config import settings
 
 ollama_llm = ChatOllama(
-    model=settings.llm.ollama.model,
-    base_url=settings.llm.ollama.base_url,
+    model=settings.ai.llm.ollama.model,
+    base_url=settings.ai.llm.ollama.base_url,
 )
