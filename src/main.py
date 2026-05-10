62e16c3d06044da9ba1060c9e8fca2ed95153b33
eugene <eugeneliukin.dev@gmail.com>
2026-05-10 22:11:41 +0300

    include rag router

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from api.v1.rag import router
 from core.db import db_manager
 
 
@@ -13,3 +14,4 @@ async def lifespan(_app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(router)
