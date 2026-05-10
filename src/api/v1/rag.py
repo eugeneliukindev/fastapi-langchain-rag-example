81288342aca0a80fe5106d7270e1e7f4a4bc17af
eugene <eugeneliukin.dev@gmail.com>
2026-05-10 22:11:33 +0300

    add rag api

---
+++ src/api/v1/rag.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query, UploadFile
+
+from service.rag import RagService
+
+router = APIRouter(
+    prefix="/rag",
+    tags=["RAG"],
+)
+
+
+@router.post("upload-pdf")
+async def upload_pdf(file: UploadFile, service: Annotated[RagService, Depends()]):
+    await service.upload_pdf(file)
+
+
+@router.get("ask-pdf")
+async def ask_pdf(q: Annotated[str, Query(min_length=1)], service: Annotated[RagService, Depends()]):
+    return await service.ask_pdf(q)
