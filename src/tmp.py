01c53906077cb6eee3fd3340c55d54dbc3f48e79
eugene <eugeneliukin.dev@gmail.com>
2026-05-10 22:19:58 +0300

    fix double Path

--- src/tmp.py
+++ src/tmp.py
@@ -7,7 +7,7 @@ from tempfile import gettempdir
 
 @contextmanager
 def create_tmp_file_from_bytes(b: bytes, extension: str) -> Generator[Path]:
-    tmp_file = Path(Path(gettempdir()) / f"{token_urlsafe(32)}.{extension}")
+    tmp_file = Path(gettempdir()) / f"{token_urlsafe(32)}.{extension}"
     tmp_file.write_bytes(b)
     yield tmp_file
     tmp_file.unlink(missing_ok=True)
